90c33410cf02900b94c58dd0f4b70a312519b8c7
vaibhavpanchal205 <vaibhavpanchal205@gmail.com>
2025-01-12 11:43:34 +0530

    update

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = (setShowLogin) => {
                 <img src={assets.basket_icon} alt="" />
                 <div className="dot"></div>
             </div>
-            <button onClick={()=>setShowLogin(true)}>Sign in</button>
+            <button onClick={()=>setShowLogin(true)}>Sign</button>
         </div>
     </div>
   )
